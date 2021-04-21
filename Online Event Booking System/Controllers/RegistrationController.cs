8244572cdb045897ab666c27c5c6117bc3395491
Fritha10 <frithapahuja17@gmail.com>
2021-04-21 22:47:08 +0530

    SignUp Page Created

---
+++ Online Event Booking System/Controllers/RegistrationController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Online_Event_Booking_System.Models;
+
+namespace Online_Event_Booking_System.Controllers
+{
+    public class RegistrationController : Controller
+    {
+
+        private readonly ApplicationUser _auc;
+
+        public RegistrationController(ApplicationUser auc)
+        {
+            _auc = auc;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(User uc)
+        {
+            _auc.Add(uc);
+            _auc.SaveChanges();
+            ViewBag.message = "Details saved successfully";
+            return View();
+        }
+
+    }
+}
---
+++ Online Event Booking System/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_Event_Booking_System.Models
+{
+    public class ApplicationUser : DbContext
+    {
+        public ApplicationUser(DbContextOptions<ApplicationUser> options) : base(options)
+        {
+
+        }
+        public DbSet<User> UserRegistration { get; set; }
+    }
+
+}
